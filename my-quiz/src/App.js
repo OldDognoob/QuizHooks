7d52646ca109b15c9ff8be1720878c0073c15fff
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-02 21:10:51 +0300

    wip

--- my-quiz/src/App.js
+++ my-quiz/src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
+import QuestionsList from "./components/QuestionsList";
+import {v4 as uuidv4} from "uuid";
 import './App.css';
 
 function App() {
+  const[currentQuestion, setCurrentQuestion] = useState(0);
+  const[score,setScore] = useState(0);
   return (
-    <div className="App">
-      App
-    </div>
-  );
+    <div className="app-wrapper">
+      <div>
+        <div className="questions-section-wrapper">
+          <div className="question-count">
+            Question{currentQuestion +1} of {QuestionsList.length}
+          </div>
+          <div className="question">
+            {QuestionsList[currentQuestion].question}
+          </div>
+        </div>
+      </div>
+    </div>);
 }
 
 export default App;
