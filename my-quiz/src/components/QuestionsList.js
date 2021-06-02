0bf2009fda688949830bc6e06ed8095e7c57cf1b
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-06-02 20:52:00 +0300

    wip-project

---
+++ my-quiz/src/components/QuestionsList.js
@@ -0,0 +1,48 @@
+const QuestionsList = [
+    {
+        question:"What is the acronym OOO stands for?",
+        answerList:[
+            {answer: "Oooo Our Over", isCorrect: false},
+            {answer: "Out of Office", isCorrect: true},
+            {answer:"One on One", isCorrect: false},
+            {answer:"Out of the Ordinary", isCorrect:false}
+        ],
+    },
+    {
+        question:"Which was the first military runway in Greece?",
+        answerList:[
+            {answer:"Tanagra Airport",isCorrect:false},
+            {answer:"Axialos Airport",isCorrect:false},
+            {answer:"Larisa Airport",isCorrect:true},
+            {answer:"Benizelos Airport",isCorrect:false}
+        ],
+    },
+    {
+        question:"Who was the Primadona general in WW2?",
+        answerList:[
+            {answer:"General Bradley", isCorrect:false},
+            {answer:"General MacArthur", isCorrect:false},
+            {answer:"General Aik", isCorrect:false},
+            {answer:"General Patton", isCorrect:true}
+        ],
+    },
+    {
+        question:"What is the capital of USA?",
+        answerList:[
+            {answer:"New York", isCorrect:false},
+            {answer:"Washington DC", isCorrect:true},
+            {answer:"Cedar Rapids", isCorrect:false},
+            {answer:"Florida", isCorrect:false}
+        ],
+    },
+    {
+        question:"Who maintains React?",
+        answerList:[
+            {answer:"Google", isCorrect:false},
+            {answer:"Amazon", isCorrect:false},
+            {answer:"Facebook", isCorrect:true},
+            {answer:"Apple", isCorrect:false}
+        ],
+    },
+];
+export default QuestionsList;
